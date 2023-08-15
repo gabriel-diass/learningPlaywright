3b6c52cfa8a0c8aec7404d9e985bd63d912e3ba9
Gabriel Dias <df.leirbag@gmail.com>
2023-08-15 16:19:21 -0300

    add e2e tests

---
+++ test/e2e/e2e-payment.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test'
+
+test.describe.only('new payment', ()=>{
+    test.beforeEach(async ({ page}) => {
+        await page.goto('http://zero.webappsecurity.com/')
+        await page.click('#signin_button')
+        await page.type('#user_login', 'username')
+        await page.type('#user_password', 'password')
+        await page.click('text=Sign in')
+        await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
+
+    })
+  
+    test('should send new payment',async ({ page}) => {
+        await page.click('#pay_bills_tab')
+        await page.selectOption('#sp_payee', 'apple')
+        await page.click('#sp_get_payee_details')
+        await page.waitForSelector('#sp_payee_details')
+
+        await page.selectOption('#sp_account', '6')
+        await page.type('#sp_amount', '5000')
+        
+        await page.type('#sp_date', '2023-11-11')
+        await page.type('#sp_description', 'random message loren ipsun')
+        await page.click('#pay_saved_payees')
+
+        const message = page.locator('#alert_content > span')
+        await expect(message).toBeVisible()
+        await expect(message).toContainText('The payment was successfully submitted')
+
+
+
+    })
+})
---
+++ test/e2e/e2e-transfer-funds.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('transfer funds and make payments', ()=>{
+    test.beforeEach(async ({ page}) => {
+        await page.goto('http://zero.webappsecurity.com/')
+        await page.click('#signin_button')
+        await page.type('#user_login', 'username')
+        await page.type('#user_password', 'password')
+        await page.click('text=Sign in')
+        await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
+
+    })
+  
+    test('transfer funds',async ({ page}) => {
+        await page.click('#transfer_funds_tab')
+        await page.selectOption('#tf_fromAccountId', '2')
+        await page.selectOption('#tf_toAccountId', '3')
+        await page.type('#tf_amount', '500')
+        await page.type('#tf_description', 'description test message')
+        await page.click('#btn_submit')
+
+        const boardHeader = await page.locator('h2.board-header')
+        await expect(boardHeader).toContainText('Verify')
+        await page.click('#btn_submit')
+
+        const message = await page.locator('.alert-success')
+        await expect(message).toContainText('You successfully submitted your transaction')
+    })
+})
